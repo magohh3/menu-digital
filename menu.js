92e63034da38ba73d677bcbbfeb28326d80c0f5e
magohh3 <margarita.hernandez@pendulo.com>
2021-12-17 17:01:10 -0600

    cambio de menu

---
+++ menu.js
@@ -0,0 +1,29 @@
+var time = new Date();
+
+var hora = time.getHours();
+var dia = time.getDate();
+
+ console.log(hora);
+ console.log(dia);
+
+
+if(time > 1 && time < 5){
+  console.log('Mostrar');
+}
+
+if (hora >= 13 && hora <=17){
+  console.log('Mostrar');
+}else{
+  console.log('No mostrar');
+}
+
+switch (dia){
+  case 17 :
+  console.log('Menu3');
+  break;
+  case 18 :
+  console.log('Menu4');
+  break
+  default :
+  console.log('Sin menu');
+}
